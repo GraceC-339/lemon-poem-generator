9ce81b19f82cd2ad43b288f87e7229bb67f5aa26
Grace C <huanglingchen.grace@gmail.com>
2025-01-23 15:23:09 +0000

    Add OpenAI connection & test endpoint

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from "express";
+import chatCompletion from "./services/openai.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from my lovely new server!");
+});
+
+app.get("/test", async (req, res) => {
+  const response = await chatCompletion(
+    "Write a poem for my friend - Su. She's a lovely girl and recently she's a little bit down."
+  );
+  res.send(response);
 });
 
 export default app;
