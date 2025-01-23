e59145f2ada8ae8c1af748fcaf46d592f67166cd
Grace C <huanglingchen.grace@gmail.com>
2025-01-23 15:35:50 +0000

    Add public HTML & get-poem endpoint

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ import chatCompletion from "./services/openai.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from my lovely new server!");
-});
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from my lovely new server!");
+// });
 
 app.get("/test", async (req, res) => {
   const response = await chatCompletion(
     "Write a poem for my friend - Su. She's a lovely girl and recently she's a little bit down."
   );
   res.send(response);
+});
+
+app.get("/get-poem", async (req, res) => {
+  const response = await chatCompletion(
+    "Write a short, expressive poem about lemons, as if you are hungry. Just four lines!"
+  );
+  res.send({ poem: response });
 });
 
 export default app;
